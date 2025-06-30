37726c1f498049eeee3a10b9d63a75c563eb1c55
Janani V <118711132+Janviswa@users.noreply.github.com>
2025-06-30 08:58:53 +0530

    Add files via upload

---
+++ directkeys.py
@@ -0,0 +1,68 @@
+import ctypes
+import time
+
+SendInput = ctypes.windll.user32.SendInput
+
+# Virtual key codes
+right_pressed = 0x4D    # 'M' - Accelerate
+left_pressed = 0x4B     # 'K' - Brake
+nitro_pressed = 0xA0    # Left Shift - Nitro
+
+# C struct redefinitions
+PUL = ctypes.POINTER(ctypes.c_ulong)
+
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                ("mi", MouseInput),
+                ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+# Press key
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
+    SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+# Release key
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
+    SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+# Demo: Press Accelerate + Nitro alternately every 1 second
+if __name__ == '__main__':
+    while True:
+        print("Accelerate + Nitro")
+        PressKey(right_pressed)      # Accelerate
+        PressKey(nitro_pressed)      # Nitro
+        time.sleep(1)
+        ReleaseKey(nitro_pressed)
+        ReleaseKey(right_pressed)
+        time.sleep(1)
